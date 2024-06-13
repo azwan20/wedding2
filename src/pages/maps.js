0f68a48da9f4aa0e7e0014c7bef18b034654a246
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2024-06-13 17:25:44 +0800

    Update maps.js

--- src/pages/maps.js
+++ src/pages/maps.js
@@ -5,7 +5,6 @@ export default function Maps() {
         <>
             <div className="maps">
                 <section className="map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7947.185772160316!2d119.4275203!3d-5.1689999!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbee2b66bf04dbb%3A0x1fed8c8d21d796ce!2sCLARO%20HOTEL%20MAKASSAR!5e0!3m2!1sid!2sid!4v1718270041974!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </section>
                 <section>
                     <p>Hotel Amsterdam</p>
