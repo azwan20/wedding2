bc76bcafdcfd52e3101c2b0dd862334eaa4e7d58
Azwan Triyadi <80760932+azwan20@users.noreply.github.com>
2024-06-13 18:04:04 +0800

    Add files via upload

--- src/pages/maps.js
+++ src/pages/maps.js
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import Navbar from "./navbar";
 
 export default function Maps() {
@@ -5,13 +6,19 @@ export default function Maps() {
         <>
             <div className="maps">
                 <section className="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3973.592886112729!2d119.43022507466058!3d-5.1689998948083815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dbee2b66bf04dbb%3A0x1fed8c8d21d796ce!2sCLARO%20HOTEL%20MAKASSAR!5e0!3m2!1sid!2sid!4v1718272108004!5m2!1sid!2sid"
+                        style={{ border: '0', flex: '1 1 0%', borderRadius: '10px', width: '-webkit-fill-available', height: '-webkit-fill-available' }}
+                        allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                 </section>
                 <section>
                     <p>Hotel Amsterdam</p>
                     <p>Jl. Jendral Sudirman, No. 10</p>
                     <p>Ambon, Maluku</p>
                 </section>
-                <button>Petunjuk Lokasi</button>
+                <Link href="https://maps.app.goo.gl/jXpHTxpWjjzeAfa98">
+                    <button>Petunjuk Lokasi</button>
+                </Link>
+
             </div>
         </>
     )
--- src/pages/quran.js
+++ src/pages/quran.js
@@ -4,15 +4,15 @@ export default function Quran() {
     return (
         <>
             <div className="quran">
-            <img src="https://akcdn.detik.net.id/community/media/visual/2020/03/17/2dea2ce0-842e-467c-bb89-0bbe43eb2ef0.jpeg?q=90&w=480" width={100} />
-            <div className="d-flex flex-column ms-3 px-5 justify-content-center align-items-center" style={{paddingTop: '450px'}}>
-            <section className="mb-3">
-                    <p className="fw-bold lh-lg">Baarokalaahu laka wabaaroka 'alaika wajama'a bainakumaa fii khoirin.</p>
-                </section>
-                <section>
-                    <p>Artinya: "Semoga Allah memberkahimu di waktu bahagia dan memberkahimu di waktu susah, dan semoga Allah menyatukan kalian dalam kebaikan."</p>
-                </section>
-            </div>
+                <img src="https://akcdn.detik.net.id/community/media/visual/2020/03/17/2dea2ce0-842e-467c-bb89-0bbe43eb2ef0.jpeg?q=90&w=480" width={100} />
+                <div className="d-flex flex-column ms-3 px-5 justify-content-center align-items-center" style={{ paddingTop: '300px' }}>
+                    <section className="mb-3">
+                        <p className="fw-bold lh-lg">Baarokalaahu laka wabaaroka 'alaika wajama'a bainakumaa fii khoirin.</p>
+                    </section>
+                    <section>
+                        <p>Artinya: "Semoga Allah memberkahimu di waktu bahagia dan memberkahimu di waktu susah, dan semoga Allah menyatukan kalian dalam kebaikan."</p>
+                    </section>
+                </div>
             </div>
         </>
     )
